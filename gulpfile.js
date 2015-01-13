f2bcac2b389da78f6c14f9b1767b0ac3e3fa0f0a
Willi Klenk <willi@klenk-bondorf.de>
2015-01-13 11:15:27 +0100

    unify build - minify and uglify always

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,15 @@ var tplCache        = require('gulp-angular-templatecache');
 var jade            = require('gulp-jade');
 var plumber         = require('gulp-plumber');
 var notify          = require('gulp-notify');
+var gulpif          = require('gulp-if');
 var annotate        = require('gulp-ng-annotate');
 var uglify          = require('gulp-uglify');
 var minifycss       = require('gulp-minify-css');
 var minifyhtml      = require('gulp-minify-html');
-var gzip            = require('gulp-gzip');
 
+var prod = true;
+
+var buildDir = './dist';
 
 function errorAlert(error){
   notify.onError({title: "Gulp Error", message: "<%= error.message %>", sound: "Sosumi"})(error);
@@ -23,7 +26,9 @@ function errorAlert(error){
   this.emit("end");
 }
 
-var buildDir = './dist';
+gulp.task('clean', function(done) {
+    del([buildDir], done);
+});
 
 gulp.task('wkChartsDoc', function() {
   try {
@@ -35,27 +40,16 @@ gulp.task('wkChartsDoc', function() {
   }
 });
 
-gulp.task('clean', function(done) {
-  del([buildDir], done);
-});
-
-gulp.task('rebuild', function () {
-    runSequence('wkChartsJs', 'wkChartsCss','wkChartsDoc')
-})
-
-gulp.task('watch', function() {
-    gulp.watch(['docs/**/*.ngdoc'], ['wkChartsDoc', 'wkChartsDocCss']);
-    gulp.watch(['app/**/*.js','app/**/*.coffee','app/**/*.jade'], ['rebuild']);
-    gulp.watch(['app/**/*.css'], ['wkChartsCss']);
+gulp.task('wkChartDocsMin', function() {
+    return gulp.src(buildDir + '/docs/**/*.html',{base:buildDir})
+        .pipe(minifyhtml())
+        .pipe(gulp.dest(buildDir + '/docs/css'))
 });
 
 gulp.task('wkChartsDocCss', function() {
   return gulp.src('./docs/css/**/*.css')
-      .pipe(gulp.dest('./dist/docs/css'));
-});
-
-gulp.task('default', function(cb) {
-  runSequence('clean', 'wkChartsJs', 'wkChartsCss', 'wkChartsDoc', 'wkChartsDocCss',   cb);
+      .pipe(gulpif(prod, minifycss()))
+      .pipe(gulp.dest(buildDir + '/docs/css'));
 });
 
 gulp.task('wkChartsJs', function() {
@@ -77,8 +71,11 @@ gulp.task('wkChartsJs', function() {
       .pipe(plumber({errorHandler: errorAlert}))
       .pipe(sourcemaps.init());
 
+  //merge things together
   return es.merge(csJs, js, templ)
       .pipe(concat('wk-charts.js'))
+      .pipe(gulpif(prod, annotate()))
+      .pipe(gulpif(prod, uglify()))
       .pipe(sourcemaps.write('./maps'))
       .pipe(gulp.dest(buildDir + '/lib'))
 });
@@ -87,58 +84,23 @@ gulp.task('wkChartsCss', function() {
     // watch wk-charts project for changes
     return gulp.src('./app/**/*.css',{base:'./'})
         .pipe(plumber({errorHandler: errorAlert}))
+        .pipe(sourcemaps.init())
         .pipe(concat('wk-charts.css'))
+        .pipe(gulpif(prod, minifycss()))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(buildDir + '/lib'))
 });
 
-gulp.task('prodClean', function(done) {
-    del(['./prod/'], done);
+gulp.task('default', function(cb) {
+    runSequence('clean', 'wkChartsJs', 'wkChartsCss', 'wkChartsDoc', 'wkChartsDocCss', 'wkChartDocsMin',  cb);
 });
 
-gulp.task('prodJs', function() {
-    var csJs = gulp.src('./app/**/*.coffee',{base:'/'})
-        .pipe(plumber({errorHandler: errorAlert}))
-        .pipe(coffee({bare:true, doctype:'html'}));
-
-    // Jade templates
-    var templ = gulp.src('./app/**/*.jade')
-        .pipe(plumber({errorHandler: errorAlert}))
-        .pipe(jade({pretty:true, doctype:'html'}))
-        .pipe(tplCache({module:'wk.chart'}));
-
-    // Javascript components
-    var js = gulp.src('./app/**/*.js',{base:'/'})
-        .pipe(plumber({errorHandler: errorAlert}))
-
-    return es.merge(csJs, js, templ)
-        .pipe(concat('wk-charts.js'))
-        .pipe(annotate())
-        .pipe(uglify())
-        .pipe(gulp.dest('./prod/lib'))
-});
-
-gulp.task('prodCss', function() {
-    // watch wk-charts project for changes
-    return gulp.src('./app/**/*.css',{base:'./'})
-        .pipe(plumber({errorHandler: errorAlert}))
-        .pipe(concat('wk-charts.css'))
-        .pipe(minifycss())
-        .pipe(gulp.dest('./prod/lib'))
-});
-
-gulp.task('prodDocCss', function() {
-    return gulp.src(buildDir + '/docs/**/*.css',{base:buildDir + '/docs'})
-        .pipe(minifycss())
-        .pipe(gulp.dest('./prod/lib/docs'))
-
+gulp.task('rebuild', function(cb) {
+    runSequence('wkChartsJs', 'wkChartsCss', 'wkChartsDoc', 'wkChartsDocCss', 'wkChartDocsMin',  cb);
 })
 
-gulp.task('prodDocs', function() {
-    return gulp.src(buildDir + '/docs/**/*.html',{base:buildDir})
-        .pipe(minifyhtml())
-        .pipe(gulp.dest('./prod/lib'))
-});
-
-gulp.task('prodBuild', function(cb) {
-    runSequence('prodClean', ['prodJs', 'prodCss', 'prodDocs', 'prodDocCss'],cb)
+gulp.task('watch', function() {
+    gulp.watch(['docs/**/*.ngdoc'], ['wkChartsDoc', 'wkChartsDocCss']);
+    gulp.watch(['app/**/*.js','app/**/*.coffee','app/**/*.jade'], ['rebuild']);
+    gulp.watch(['app/**/*.css'], ['wkChartsCss']);
 });
